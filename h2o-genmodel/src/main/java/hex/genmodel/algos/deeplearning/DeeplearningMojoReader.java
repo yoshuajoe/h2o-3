b7367b7e38ec2e2f18a7f7e6068be15538b0e324
wendycwong <wendycwong@yahoo.com>
2017-08-31 19:44:36 -0700

    Fixed bug in smoke.

--- h2o-genmodel/src/main/java/hex/genmodel/algos/deeplearning/DeeplearningMojoReader.java
+++ h2o-genmodel/src/main/java/hex/genmodel/algos/deeplearning/DeeplearningMojoReader.java
@@ -21,8 +21,8 @@ public class DeeplearningMojoReader extends ModelMojoReader<DeeplearningMojoMode
     _model._nums = readkv("nums");
     _model._cats = readkv("cats");
     _model._catoffsets = readkv("cat_offsets");
-    _model._normmul = readkv("norm_mul");
-    _model._normsub = readkv("norm_sub");
+    _model._normmul = readkv("norm_mul", new double[0]);
+    _model._normsub = readkv("norm_sub", new double[0]);
     _model._normrespmul = readkv("norm_resp_mul");
     _model._normrespsub = readkv("norm_resp_sub");
     _model._use_all_factor_levels = readkv("use_all_factor_levels");
@@ -30,19 +30,19 @@ public class DeeplearningMojoReader extends ModelMojoReader<DeeplearningMojoMode
     _model._imputeMeans = readkv("mean_imputation");
     _model._family = DistributionFamily.valueOf((String)readkv("distribution"));
     if (_model._imputeMeans & (_model._cats > 0)) {
-      _model._catNAFill = readkv("cat_modes");
+      _model._catNAFill = readkv("cat_modes", new int[0]);
     }
-    _model._units = readkv("neural_network_sizes");
-    _model._all_drop_out_ratios = readkv("hidden_dropout_ratios");
+    _model._units = readkv("neural_network_sizes", new int[0]);
+    _model._all_drop_out_ratios = readkv("hidden_dropout_ratios", new double[0]);
 
     // read in biases and weights for each layer
     int numLayers = _model._units.length-1; // exclude the output nodes.
     _model._bias = new DeeplearningMojoModel.StoreWeightsBias[numLayers];
     _model._weights = new DeeplearningMojoModel.StoreWeightsBias[numLayers];
     for (int layerIndex = 0; layerIndex < numLayers; layerIndex++) {
-      double[] temp = readkv("bias_layer"+layerIndex);
+      double[] temp = readkv("bias_layer"+layerIndex, new double[0]);
       _model._bias[layerIndex] = new DeeplearningMojoModel.StoreWeightsBias(temp);
-      temp = readkv("weight_layer"+layerIndex);
+      temp = readkv("weight_layer"+layerIndex, new double[0]);
       _model._weights[layerIndex] = new DeeplearningMojoModel.StoreWeightsBias(temp);
     }
 
